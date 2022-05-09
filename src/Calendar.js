006132cc7b6e871bcd032d2e108c038aac8967ca
Sam Hendricks <samuelbhendricks@gmail.com>
2022-05-09 11:10:53 -0400

    created Calendar.js

---
+++ src/Calendar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import bootstrap from 'bootstrap';
+
+function Calendar() {
+    return (<div class="container">
+        <div class="row">
+            <div class="col">Sun</div>
+            <div class="col">Mon</div>
+            <div class="col">Tue</div>
+            <div class="col">Wed</div>
+            <div class="col">Thu</div>
+            <div class="col">Fri</div>
+            <div class="col">Sat</div>
+        </div>
+        <div class="row">
+            <div class="col">Sun</div>
+            <div class="col">Mon</div>
+            <div class="col">Tue</div>
+            <div class="col">Wed</div>
+            <div class="col">Thu</div>
+            <div class="col">Fri</div>
+            <div class="col">Sat</div>
+        </div>
+    </div>)
+}
+
+export default Calendar;
