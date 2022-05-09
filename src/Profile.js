2c980fbde170577c99589775829292649a56dd81
CatherineHonors <cathonors@gmail.com>
2022-05-09 11:15:34 -0400

    Added profile branch and basic return statement

---
+++ src/Profile.js
@@ -0,0 +1,33 @@
+import react from './react'
+
+function Profile () {
+
+
+
+
+return (
+    <div>
+        <div>
+            <h2>John Smith</h2>
+            <img alt="happy man" src="https://image.shutterstock.com/shutterstock/photos/190821629/display_1500/stock-photo-happy-man-with-white-teeth-smiling-with-thumbs-up-isolated-on-a-white-background-190821629.jpg"></img>
+            <h4>Bio</h4>
+            <p>Hey my name is John and I like pretty much all types of music, I'm very cool</p>
+            <h4>Fav Genre: Guitar</h4>
+            <button>Edit Profile</button>
+        </div>
+        <div>
+            <h2>Top 5 Albums</h2>
+            <div>
+                <img src="https://upload.wikimedia.org/wikipedia/en/2/27/Daft_Punk_-_Discovery.png"></img>
+            </div>
+            <div>
+                <p>Where calendar div will go</p>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+
+
+export default Profile
