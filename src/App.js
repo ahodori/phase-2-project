94f2b9b89052db16d73f0e3d1361f552313d26eb
Sam Hendricks <samuelbhendricks@gmail.com>
2022-05-09 13:18:39 -0400

    added react-router-dom, set up basic routes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import {Route, Routes } from "react-router-dom";
+
 import './App.css';
 import Calendar from './Calendar';
 import Profile from './Profile';
@@ -7,9 +10,13 @@ import Feed from './Feed';
 function App() {
   return (
     <div className="App">
-      <Calendar/>
-      <Profile />
-      <Feed />
+      
+      <Routes>
+        <Route path="/" element={<Calendar/>}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+        <Route path="/feed" element={<Feed />}></Route>
+      </Routes>
+
     </div>
   );
 }
