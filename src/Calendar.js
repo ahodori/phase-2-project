d17df38d17ba9e3c63fc4c6d36c1261e9ab19fc4
Sam Hendricks <samuelbhendricks@gmail.com>
2022-05-09 11:29:40 -0400

    added rows & columns

--- src/Calendar.js
+++ src/Calendar.js
@@ -21,6 +21,33 @@ function Calendar() {
             <div class="col">Fri</div>
             <div class="col">Sat</div>
         </div>
+        <div class="row">
+            <div class="col">Sun</div>
+            <div class="col">Mon</div>
+            <div class="col">Tue</div>
+            <div class="col">Wed</div>
+            <div class="col">Thu</div>
+            <div class="col">Fri</div>
+            <div class="col">Sat</div>
+        </div>
+        <div class="row">
+            <div class="col">Sun</div>
+            <div class="col">Mon</div>
+            <div class="col">Tue</div>
+            <div class="col">Wed</div>
+            <div class="col">Thu</div>
+            <div class="col">Fri</div>
+            <div class="col">Sat</div>
+        </div>
+        <div class="row">
+            <div class="col">Sun</div>
+            <div class="col">Mon</div>
+            <div class="col">Tue</div>
+            <div class="col">Wed</div>
+            <div class="col">Thu</div>
+            <div class="col">Fri</div>
+            <div class="col">Sat</div>
+        </div>
     </div>)
 }
 
