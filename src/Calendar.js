a4f8cb7876c667a51a08b153573b408d5d9ad4b2
Sam Hendricks <samuelbhendricks@gmail.com>
2022-05-09 11:40:23 -0400

    created reusable function returning a week of calendar

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,53 +1,29 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+function CalendarWeek() {
+    return (
+    <div class="row">
+        <div class="col">Sun</div>
+        <div class="col">Mon</div>
+        <div class="col">Tue</div>
+        <div class="col">Wed</div>
+        <div class="col">Thu</div>
+        <div class="col">Fri</div>
+        <div class="col">Sat</div>
+    </div>
+    )
+}
+
+
 function Calendar() {
     return (<div class="container">
-        <div class="row">
-            <div class="col">Sun</div>
-            <div class="col">Mon</div>
-            <div class="col">Tue</div>
-            <div class="col">Wed</div>
-            <div class="col">Thu</div>
-            <div class="col">Fri</div>
-            <div class="col">Sat</div>
-        </div>
-        <div class="row">
-            <div class="col">Sun</div>
-            <div class="col">Mon</div>
-            <div class="col">Tue</div>
-            <div class="col">Wed</div>
-            <div class="col">Thu</div>
-            <div class="col">Fri</div>
-            <div class="col">Sat</div>
-        </div>
-        <div class="row">
-            <div class="col">Sun</div>
-            <div class="col">Mon</div>
-            <div class="col">Tue</div>
-            <div class="col">Wed</div>
-            <div class="col">Thu</div>
-            <div class="col">Fri</div>
-            <div class="col">Sat</div>
-        </div>
-        <div class="row">
-            <div class="col">Sun</div>
-            <div class="col">Mon</div>
-            <div class="col">Tue</div>
-            <div class="col">Wed</div>
-            <div class="col">Thu</div>
-            <div class="col">Fri</div>
-            <div class="col">Sat</div>
-        </div>
-        <div class="row">
-            <div class="col">Sun</div>
-            <div class="col">Mon</div>
-            <div class="col">Tue</div>
-            <div class="col">Wed</div>
-            <div class="col">Thu</div>
-            <div class="col">Fri</div>
-            <div class="col">Sat</div>
-        </div>
+
+        {CalendarWeek()}
+        {CalendarWeek()}
+        {CalendarWeek()}
+        {CalendarWeek()}
+        {CalendarWeek()}
     </div>)
 }
 
