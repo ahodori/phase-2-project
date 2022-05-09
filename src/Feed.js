19f67e4b80cef12b8d44c9d67009b50d4d7ac913
CatherineHonors <cathonors@gmail.com>
2022-05-09 11:29:31 -0400

    added Feed component and imports into App

---
+++ src/Feed.js
@@ -0,0 +1,27 @@
+import react from './react'
+
+function Feed() {
+
+
+
+
+
+
+
+
+    return (
+        <div>
+            <div>
+                <h1>Album List</h1>
+                <button>Search Bar</button>
+            </div>
+            <div>
+                <ul>
+                <li>single album goes here</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Feed;
