c1ad9fb5040f3fc4f83b48d4dbc885ba467fc695
CatherineHonors <cathonors@gmail.com>
2022-05-10 10:54:03 -0400

    delete button

--- src/AlbumCard.js
+++ src/AlbumCard.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 
-function AlbumCard({title, artist, dateAdded, image, rating, comments}) {
+function AlbumCard({title, artist, dateAdded, image, rating, comments, id, filteredDeletedAlbum}) {
 
-
-
-
+    const handleDelete = () => {
+        fetch(`http://localhost:3000/albumEntries/${id}`, {
+            method:'DELETE',
+        })
+        filteredDeletedAlbum(id)
+    }
+    
     return (
         <div>
             <h1>{title}</h1>
@@ -16,6 +20,7 @@ function AlbumCard({title, artist, dateAdded, image, rating, comments}) {
             {comments.map((comment) => {
                 return(<p><span>{comment.username}: </span>{comment.comment}</p>)
             })}
+            <button onClick={handleDelete} className="albumcarddelete">Delete Album</button>
         </div>
     )
 }
